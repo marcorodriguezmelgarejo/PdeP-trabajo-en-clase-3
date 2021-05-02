cc7547b8ab8f3dda3dbdb97b23f628773e61e21a
MarcoRodriguezMelgarejo <mrodrguezmelgarejo@frba.utn.edu.ar>
2021-05-01 22:14:43 -0300

    cambio de nombre tests

--- src/Spec.hs
+++ src/Spec.hs
@@ -6,12 +6,12 @@ import Test.Hspec
 correrTests :: IO ()
 correrTests = hspec $ do
   describe "tests objetivoDificil" $ do
-    it "bolsa de entrenamiento es difícil" $ do
+    it "cadena larga es difícil" $ do
       objetivoDificil "bolsa de entrenamiento" `shouldBe` True
-    it "puf es dificil" $ do
+    it "cadena corta no es difícil" $ do
       objetivoDificil "puf" `shouldBe` False
   describe "tests objetivoAccesible" $ do
-    it "bolsa de entrenamiento es accesible" $ do
+    it "cadena larga es accesible" $ do
       objetivoAccesible "bolsa de entrenamiento" `shouldBe` True
-    it "puf no es accesible" $ do
+    it "cadena corta no es accesible" $ do
       objetivoAccesible "puf" `shouldBe` False
